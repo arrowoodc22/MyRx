7af2c46b90a2c7b2454085f0c690a3fc3717c5c3
arrowoodc22 <arrowoodc22@hanover.edu>
2021-09-28 10:28:42 -0400

    MyRx Initial Commit.

---
+++ app/src/main/java/com/example/bottomnavigationworkingproperly/ui/manage/ManageFragment.java
@@ -0,0 +1,45 @@
+package com.example.bottomnavigationworkingproperly.ui.manage;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.bottomnavigationworkingproperly.databinding.FragmentManageBinding;
+
+public class ManageFragment extends Fragment {
+
+    private ManageViewModel manageViewModel;
+    private FragmentManageBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        manageViewModel =
+                new ViewModelProvider(this).get(ManageViewModel.class);
+
+        binding = FragmentManageBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textManage;
+        manageViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
---
+++ app/src/main/java/com/example/bottomnavigationworkingproperly/ui/manage/ManageViewModel.java
@@ -0,0 +1,19 @@
+package com.example.bottomnavigationworkingproperly.ui.manage;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class ManageViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public ManageViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is manage fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
